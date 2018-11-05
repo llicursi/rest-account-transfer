18e7d905887f0aba72f124abe687cb7dea38d097
Licursi, Lucas <llicursi@gmail.com>
2018-11-04 22:56:07 -0200

    Initial repository setup created via Spring initializr

---
+++ src/main/java/com/licursi/rest/transferservice/TransferServiceApplication.java
@@ -0,0 +1,12 @@
+package com.licursi.rest.transferservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/licursi/rest/transferservice/TransferServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.licursi.rest.transferservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TransferServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
