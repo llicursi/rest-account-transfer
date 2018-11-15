d3e69e2ee10cdb93efd1721f32cdd11502772cf6
Licursi, Lucas <llicursi@gmail.com>
2018-11-15 17:07:22 -0200

    Adjusts the error api and exceptions

--- src/main/java/com/licursi/rest/transferservice/exceptions/BalanceConstraintViolationException.java
+++ src/main/java/com/licursi/rest/transferservice/exceptions/BalanceConstraintViolationException.java
@@ -1,9 +1,15 @@
 package com.licursi.rest.transferservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
- *  Throws whenever the balance reaches an unexpected condition.
+ * Throws whenever the balance reaches an unexpected condition.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class BalanceConstraintViolationException extends Exception {
+    private static final long serialVersionUID = -5483777273652169678L;
+
     public BalanceConstraintViolationException(String message) {
         super(message);
     }
--- src/main/java/com/licursi/rest/transferservice/exceptions/NegativeConstraintViolationException.java
+++ src/main/java/com/licursi/rest/transferservice/exceptions/NegativeConstraintViolationException.java
@@ -1,9 +1,15 @@
 package com.licursi.rest.transferservice.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Throws on any attempt of using negative value on Account operations
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NegativeConstraintViolationException extends Exception {
+
+    private static final long serialVersionUID = 4760963348906594536L;
 
     public NegativeConstraintViolationException(String message) {
         super(message);
